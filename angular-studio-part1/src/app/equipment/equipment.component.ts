335e02977d17ec78336ba280a260bced22345196
Natasha Matthews <natashamatthews90@gmail.com>
2021-12-06 19:50:15 -0600

    added all the components

--- angular-studio-part1/src/app/equipment/equipment.component.ts
+++ angular-studio-part1/src/app/equipment/equipment.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-equipment',
-  templateUrl: './equipment.component.html',
-  styleUrls: ['./equipment.component.css']
+  selector: "app-equipment",
+  templateUrl: "./equipment.component.html",
+  styleUrls: ["./equipment.component.css"],
 })
 export class EquipmentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
